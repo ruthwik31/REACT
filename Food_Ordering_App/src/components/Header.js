75f1e02dbcb459838e5909597d6e97182c087f92
RUTHWIK <ruthwik4566@gmail.com>
2025-06-23 22:50:20 +0530

    added login logout button

--- Food_Ordering_App/src/components/Header.js
+++ Food_Ordering_App/src/components/Header.js
@@ -1,5 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
 const Header = () => {
+  //let btnName = "Login";
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="Logo-Container">
@@ -11,6 +15,14 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnNameReact==="Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
