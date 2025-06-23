975857a6e7ce2751a5335fb1eeee52d596513e80
RUTHWIK <ruthwik4566@gmail.com>
2025-06-23 22:52:19 +0530

    added shimmer

---
+++ Food_Ordering_App/src/components/shimmer.js
@@ -0,0 +1,10 @@
+const Shimmerui=()=>{
+    return(
+        <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        </div>
+    );
+};
+export default Shimmerui;
