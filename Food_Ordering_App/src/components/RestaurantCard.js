6a8634b7c1fef35f946fc51af03c143cb7ec302e
RUTHWIK <ruthwik4566@gmail.com>
2025-06-23 12:15:04 +0530

    fixed fetech error

--- Food_Ordering_App/src/components/RestaurantCard.js
+++ Food_Ordering_App/src/components/RestaurantCard.js
@@ -2,8 +2,8 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { name, avgRating, cuisines, deliveryTime, cloudinaryImageId } =
-    resData?.data;
+  const { name, avgRating, cuisines, sla, cloudinaryImageId } =
+    resData?.info;
   return (
     <div className="restaurant-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
@@ -14,7 +14,7 @@ const RestaurantCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>Rating: {avgRating}</h4>
-      <h4>delivery Time:{deliveryTime}</h4>
+      <h4>delivery Time:{sla?.slaString}</h4>
     </div>
   );
 };
